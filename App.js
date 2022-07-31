372733beabda70301e89eb56e896af7e789f41c5
johnbuenodev <johnbuenodev@gmail.com>
2022-07-31 14:21:08 -0300

    Exemplo de flexbox alinhamentos/posições

--- App.js
+++ App.js
@@ -2,19 +2,52 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.containerRowStretch}>
+      <View style={{width:'100px', height:'50px', backgroundColor: 'green'}}></View>
+      <View style={{width:'50px', height:'50px', backgroundColor: 'purple'}}></View>
+      <View style={{width:'50px', height:'50px', backgroundColor: 'yellow'}}></View>
     </View>
   );
 }
 
+/*
+
+    <View style={styles.containerRowCenter}>
+      <View style={{width:'50px', height:'50px', backgroundColor: 'green'}}></View>
+      <View style={{width:'50px', height:'50px', backgroundColor: 'purple'}}></View>
+      <View style={{width:'50px', height:'50px', backgroundColor: 'yellow'}}></View>
+    </View>
+
+    <View style={styles.containerRowStretch}>
+      <View style={{width:'100px', height:'50px', backgroundColor: 'green'}}></View>
+      <View style={{width:'50px', height:'50px', backgroundColor: 'purple'}}></View>
+      <View style={{width:'50px', height:'50px', backgroundColor: 'yellow'}}></View>
+    </View>
+
+*/
+
 const styles = StyleSheet.create({
-  container: {
+  containerRowSpaceBetween: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'row', 
+    justifyContent:'space-between'
+  },
+  containerRowCenter: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
     justifyContent: 'center',
   },
+  containerRowStretch: {
+    flex: 1,
+    backgroundColor: '#fff',
+    //start center end
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'stretch',
+  }
 });
+
